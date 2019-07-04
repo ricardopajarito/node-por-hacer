e9ba8e8b95d7f4a46a6af2fa38f5e6036a49f9c1
Ricardo <ricardopch15@gmail.com>
2019-07-03 19:00:05 -0500

    Primer commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## Aplicacion de comandos ejercicio del curso
+
+Instalar los paquetes de node
+
+...
+npm install
+...
+
+node app crear -d "Nota"
+node app listar
+node app actualizar -d "Nota" -c true
+node app borrar -d "Nota"
---
+++ app.js
@@ -0,0 +1,39 @@
+const argv = require('./config/yargs').argv;
+const porHacer = require('./por-hacer/por-hacer');
+const colors = require('colors');
+
+//console.log(argv);
+
+let comando = argv._[0];
+
+switch (comando) {
+    case 'crear':
+        //console.log('Crear por hacer');
+        let tarea = porHacer.crear(argv.descripcion);
+        console.log(tarea);
+        break;
+    case 'listar':
+        //console.log('Mostrar todas las tareas por hacer');
+        let listado = porHacer.getListado();
+
+        for (let tarea of listado) {
+            console.log('=========Por Hacer=========='.green);
+            console.log(tarea.descripcion);
+            console.log('Estado: ', tarea.completado);
+            console.log('==================='.green);
+        }
+        break;
+    case 'actualizar':
+        //console.log('Actualizar una tarea por hacer');
+        let actualizado = porHacer.actualizar(argv.descripcion, argv.completado);
+        console.log(actualizado);
+
+        break;
+    case 'borrar':
+        //console.log('Actualizar una tarea por hacer');
+        let borrado = porHacer.borrar(argv.descripcion);
+        console.log(borrado);
+        break;
+    default:
+        console.log('Comando no reconocido');
+}
